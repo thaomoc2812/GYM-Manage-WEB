fcdda82b5addac283f78b643dba94f812abd756f
Mộc <94171726+thaomoc2812@users.noreply.github.com>
2023-07-02 00:31:53 +0700

    Update createTaiKhoanNhanVien.php

--- php/quanLyNhanVien/createTaiKhoanNhanVien.php
+++ php/quanLyNhanVien/createTaiKhoanNhanVien.php
@@ -5,15 +5,25 @@
 //nhan du lieu tu form
 $sdt = $_POST['sdt'];
 $matkhau = $_POST['matkhau'];
+$ten = $_POST['ten'];
+$diachi = $_POST['diachi'];
+$vitri = $_POST['vitri'];
 
 
 //ket noi csdl
 require_once '../connect.php';
 
 
+if (!$sdt || !$matkhau || !$ten || !$diachi || !$vitri)
+    {
+        echo "Vui lòng nhập đầy đủ thông tin. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
 
-
-
+    if (!preg_match("/^[0-9]{10}$/", $sdt)) {
+        echo "Số điện thoại không hợp lệ. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
+        exit;
+    }
 
 
 $search_sql = "SELECT * FROM nhanvien WHERE 
@@ -38,17 +48,26 @@ while ($r = mysqli_fetch_assoc($result))
 
 if($q == 0)
 {
-    echo "<h1>Chưa có nhân viên này trong danh sách</h1>";
+    $addsql = "INSERT INTO nhanvien
+    (ten, diachi, sdt,vitri,matkhau) VALUES ('$ten','$diachi','$sdt','$vitri','$matkhau')";
+    //thuc thi cau lenh them
+    if(mysqli_query($conn, $addsql)){
+        
+        echo "<h1>Tạo tài khoản thành công</h1>";
+
+    };
 }
 
 else
 {
-    $addsql = "UPDATE nhanvien SET matkhau = '$matkhau' WHERE sdt = $sdt";
-    if( mysqli_query($conn, $addsql)){
+    // $upsql = "UPDATE nhanvien SET matkhau = '$matkhau' WHERE sdt = $sdt";
+    // if( mysqli_query($conn, $upsql)){
     
-        echo "<h1>Tạo tài khoản thành công</h1>";
+    //     echo "<h1>Tạo tài khoản thành công</h1>";
     
-    };
+    // };
+
+    echo "<h1>Số điện thoại đã tồn tại trong hệ thống</h1>";
 }
 
 
