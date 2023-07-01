e1e80ffa4bc273f24e97a83028aec45b33a8b3e4
Mộc <94171726+thaomoc2812@users.noreply.github.com>
2023-07-02 01:03:39 +0700

    Update updateTrangThietBi.php

--- php/quanLyTrangThietBi/updateTrangThietBi.php
+++ php/quanLyTrangThietBi/updateTrangThietBi.php
@@ -13,6 +13,23 @@ $id = $_POST['sid'];
 //ket noi csdl
 require_once '../connect.php';
 
+$search = "SELECT * FROM trangthietbi WHERE (maso = '$maso') ";
+
+$query = mysqli_query($conn, $search);
+$q=0;
+while ($r0 = mysqli_fetch_assoc($query))
+{
+    if($r0['tenthietbi'] != $tenthietbi)
+    {
+        $q = 1;
+    }
+    
+};
+if($q == 1)
+{
+    echo "Mã số thiêt bị đã tồn tại <a href='javascript: history.go(-1)'>Trở lại</a>";
+    exit;
+}
 
 
 $update_sql = "UPDATE trangthietbi SET maso = '$maso', tenthietbi = '$tenthietbi', soluong = $soluong,ngaynhapve = '$ngaynhapve', ngaybaohanh = '$ngaybaohanh', xuatsu = '$xuatsu', tinhtrang = '$tinhtrang' WHERE id = $id";
