8d395c4958212bb5f84ae1d725be40ebdf6fe897
Mộc <94171726+thaomoc2812@users.noreply.github.com>
2023-07-02 00:48:40 +0700

    Update updatePhongTapStaff.php

--- php/quanLyPhongTap/updatePhongTapStaff.php
+++ php/quanLyPhongTap/updatePhongTapStaff.php
@@ -9,6 +9,23 @@ $id = $_POST['sid'];
 //ket noi csdl
 require_once '../connect.php';
 
+$search = "SELECT * FROM phongtap WHERE (maphong = '$mp') ";
+
+$query = mysqli_query($conn, $search);
+$q=0;
+while ($r0 = mysqli_fetch_assoc($query))
+{
+    if($r0['tenphong'] != $tp)
+    {
+        $q = 1;
+    }
+    
+};
+if($q == 1)
+{
+    echo "Mã phòng đã tồn tại <a href='javascript: history.go(-1)'>Trở lại</a>";
+    exit;
+}
 
 
 $update_sql = "UPDATE phongtap SET maphong = '$mp', tenphong = '$tp', soluong = $sl WHERE id = $id";
